1cbce2d840e3509b04d6627e4f85f6f66d171cc7
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2023-09-27 00:34:41 -0300

    New Excersises

--- Tarea_1.R
+++ Tarea_1.R
@@ -1,6 +1,16 @@
 #install.packages("ggplot2", dependencies = TRUE)
 #install.packages("GGally")
 #install.packages("corrplot")
+#install.packages("gridExtra")
+#install.packages("cowplot")
+#install.packages("reshape2")
+#install.packages("grid")
+#install.packages("e1071")
+library(e1071)
+library(grid)
+library(reshape2)
+library(gridExtra)
+library(cowplot)
 library(corrplot)
 library(ggplot2)
 library(GGally)
@@ -17,4 +27,58 @@ pairs(data_boston_housing, pch= 20, col= 'purple')
 
 ggpairs(data_boston_housing)
 
-# Ejercicio 4.-
+# Ejercicio 9.-
+features_data = names(data_boston_housing)
+boxplot_list = list()
+size_col = ncol(data_boston_housing)
+
+for(i in 1: size_col) {
+    feature = names(data_boston_housing)[i]
+    boxplot_data = ggplot(data_boston_housing, aes(y= .data[[feature]])) +
+                   geom_boxplot() +
+                   theme_minimal()
+    boxplot_list[[i]] = ggplotGrob(boxplot_data)
+}
+
+grid_boxplot = grid.arrange(grobs = boxplot_list)
+ggdraw() + draw_plot(grid_boxplot) + draw_plot(textGrob("Boxplot Boston Housing",
+                                                        gp= gpar(fontsize= 20)),
+                                               x= 0.27, y= -0.35)
+
+# Ejercicio 10.-
+ggplot(data_boston_housing, aes(x= .data[['medv']], y= .data[['rm']])) +
+  geom_point() +
+  geom_smooth(method=lm, se=FALSE) +
+  theme_minimal() +
+  ggtitle('MEDV V/S RM')
+
+# Ejercicio 11.-
+for(feature in features_data) {
+    asimetria = round(skewness(data_boston_housing[[feature]]), 3)
+    curtosis = round(kurtosis(data_boston_housing[[feature]]), 3)
+
+    print(feature)
+    print(paste('Asimetria: ', asimetria, ', Curtosis: ', curtosis, sep = ''))
+}
+
+# Ejercicio 12.-
+features_data = names(data_boston_housing)
+densityplot_list = list()
+size_col = ncol(data_boston_housing)
+
+for(i in 1: size_col) {
+    feature = names(data_boston_housing)[i]
+    densityplot_data = ggplot(data_boston_housing, aes(x= .data[[feature]])) +
+      geom_density(color = 4,    # Color
+                   lwd = 1,      # Ancho
+                   linetype = 1,
+                   alpha = 0.25,
+                   fill = 4) +
+      theme_minimal()
+    densityplot_list[[i]] = ggplotGrob(densityplot_data)
+}
+
+grid_densityplot = grid.arrange(grobs = densityplot_list)
+ggdraw() + draw_plot(grid_densityplot) + draw_plot(textGrob("Densityplot Boston Housing",
+                                                        gp= gpar(fontsize= 20)),
+                                               x= 0.27, y= -0.35)
