638c8ad37c94cf5de8059f7fb079c8f1cd36408e
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2023-09-27 23:41:08 -0300

    UwUntu

--- Tarea_1.R
+++ Tarea_1.R
@@ -16,16 +16,16 @@ library(ggplot2)
 library(GGally)
 
 # Ejercicio 1.-
-data_boston_housing = read.csv("BostonHousing.csv")
+data_boston_housing <- read.csv("BostonHousing.csv")
 head(data_boston_housing, n = 5)
 
 # Ejercicio 2.-
-features_data = names(data_boston_housing)
+features_data <- names(data_boston_housing)
 
 for(feature in features_data) {
-    mean_feature = mean(data_boston_housing[[feature]])
-    median_feature = median(data_boston_housing[[feature]])
-    desv_feature = sd(data_boston_housing[[feature]])
+    mean_feature <- mean(data_boston_housing[[feature]])
+    median_feature <- median(data_boston_housing[[feature]])
+    desv_feature <- sd(data_boston_housing[[feature]])
 
     cat(paste(feature, ':\n', sep = ''))
     cat(paste('Promedio:', round(mean_feature, 3), '\n'))
@@ -39,7 +39,7 @@ pairs(data_boston_housing, pch= 20, col= 'purple')
 
 # Ejercicio 4.-
 
-correlacion=cor(data_boston_housing, method= "pearson")
+correlacion <- cor(data_boston_housing, method= "pearson")
 print(correlacion)
 
 #Ejercicio 5.-
@@ -54,7 +54,7 @@ corrplot(correlacion, method = "color", type = 'lower')
 
 #Ejercicio 7.-
 
-new_data_corr = data_boston_housing[, c("indus", "nox", "age", "dis", "rad", "tax")]
+new_data_corr <- data_boston_housing[, c("indus", "nox", "age", "dis", "rad", "tax")]
 pairs(new_data_corr, lower.panel = function(x, y) {
     points(x, y)
     abline(lm(y ~ x), col = "red")
@@ -62,7 +62,7 @@ pairs(new_data_corr, lower.panel = function(x, y) {
 
 #Ejercicio 8.-
 
-numeroDeColumnas = ncol(data_boston_housing)
+numeroDeColumnas <- ncol(data_boston_housing)
 par(mfrow = c(ceiling(sqrt(numeroDeColumnas)), ceiling(sqrt(numeroDeColumnas))))
 
 for (i in 1:numeroDeColumnas) {
@@ -72,30 +72,30 @@ for (i in 1:numeroDeColumnas) {
 par(mfrow = c(1, 1))
 
 # Ejercicio 9.-
-features_data = names(data_boston_housing)
-boxplot_list = list()
-size_col = ncol(data_boston_housing)
+features_data <- names(data_boston_housing)
+boxplot_list <- list()
+size_col <- ncol(data_boston_housing)
 
 for(i in 1: size_col) {
-    feature = features_data[i]
-    boxplot_data = ggplot(data_boston_housing, aes(y= .data[[feature]])) +
+    feature <- features_data[i]
+    boxplot_data <- ggplot(data_boston_housing, aes(y= .data[[feature]])) +
                    geom_boxplot() +
                    theme_minimal()
-    boxplot_list[[i]] = ggplotGrob(boxplot_data)
+    boxplot_list[[i]] <- ggplotGrob(boxplot_data)
 }
 
-grid_boxplot = grid.arrange(grobs = boxplot_list)
+grid_boxplot <- grid.arrange(grobs = boxplot_list)
 ggdraw() + draw_plot(grid_boxplot) + draw_plot(textGrob("Boxplot Boston Housing",
                                                         gp= gpar(fontsize= 20)),
                                                x= 0.27, y= -0.35)
 
 # Ejercicio 10.-
-positive_corr = ggplot(data_boston_housing, aes(x= .data[['medv']], y= .data[['rm']])) +
+positive_corr <- ggplot(data_boston_housing, aes(x= .data[['medv']], y= .data[['rm']])) +
                   geom_point() +
                   geom_smooth(method=lm, se=FALSE) +
                   theme_minimal() +
                   ggtitle('MEDV V/S RM')
-negative_corr = ggplot(data_boston_housing, aes(x= .data[['medv']], y= .data[['lstat']])) +
+negative_corr <- ggplot(data_boston_housing, aes(x= .data[['medv']], y= .data[['lstat']])) +
                   geom_point() +
                   geom_smooth(method=lm, se=FALSE) +
                   theme_minimal() +
@@ -105,65 +105,65 @@ grid.arrange(positive_corr, negative_corr, ncol = 2)
 
 # Ejercicio 11.-
 for(feature in features_data) {
-    asimetria = round(skewness(data_boston_housing[[feature]]), 3)
-    curtosis = round(kurtosis(data_boston_housing[[feature]]), 3)
+    asimetria <- round(skewness(data_boston_housing[[feature]]), 3)
+    curtosis <- round(kurtosis(data_boston_housing[[feature]]), 3)
 
     cat(paste(feature, '\n'))
     cat(paste('Asimetria: ', asimetria, ', Curtosis: ', curtosis, '\n\n', sep = ''))
 }
 
 # Ejercicio 12.-
-features_data = names(data_boston_housing)
-densityplot_list = list()
-size_col = ncol(data_boston_housing)
+features_data <- names(data_boston_housing)
+densityplot_list <- list()
+size_col <- ncol(data_boston_housing)
 
 for(i in 1: size_col) {
-    feature = features_data[i]
-    densityplot_data = ggplot(data_boston_housing, aes(x= .data[[feature]])) +
+    feature <- features_data[i]
+    densityplot_data <- ggplot(data_boston_housing, aes(x= .data[[feature]])) +
       geom_density(color = 4,    # Color
                    lwd = 1,      # Ancho
                    linetype = 1,
                    alpha = 0.25,
                    fill = 4) +
       theme_minimal()
-    densityplot_list[[i]] = ggplotGrob(densityplot_data)
+    densityplot_list[[i]] <- ggplotGrob(densityplot_data)
 }
 
-grid_densityplot = grid.arrange(grobs = densityplot_list)
+grid_densityplot <- grid.arrange(grobs = densityplot_list)
 ggdraw() + draw_plot(grid_densityplot) + draw_plot(textGrob("Densityplot Boston Housing",
                                                         gp= gpar(fontsize= 20)),
                                                x= 0.27, y= -0.35)
 
 #Ejercicio 13.-
-grupo_1_chas= data_boston_housing[data_boston_housing$chas == 0, ]
-grupo_2_chas= data_boston_housing[data_boston_housing$chas == 1, ]
+grupo_1_chas <- data_boston_housing[data_boston_housing$chas == 0, ]
+grupo_2_chas <-  data_boston_housing[data_boston_housing$chas == 1, ]
 
 #Ejercicio 14
-datos_grupo1 = names(grupo_1_chas)
-datos_grupo2 = names(grupo_2_chas)
+datos_grupo1 <- names(grupo_1_chas)
+datos_grupo2 <- names(grupo_2_chas)
 
-resultados_grupo1 = c()
-resultados_grupo2 = c()
+resultados_grupo1 <- c()
+resultados_grupo2 <- c()
 
 for(feature in datos_grupo1) {
     if(feature != 'chas') {
-        mean_feature = mean(grupo_1_chas[[feature]])
-        var_feature = var(grupo_1_chas[[feature]])
+        mean_feature <- mean(grupo_1_chas[[feature]])
+        var_feature <- var(grupo_1_chas[[feature]])
 
-        resultados_grupo1 = c(resultados_grupo1, paste(feature, ':\n', sep = ''))
-        resultados_grupo1 = c(resultados_grupo1, paste('Media:', round(mean_feature, 3), '\n'))
-        resultados_grupo1 = c(resultados_grupo1, paste('varianza:', round(var_feature, 3), '\n\n'))
+        resultados_grupo1 <- c(resultados_grupo1, paste(feature, ':\n', sep = ''))
+        resultados_grupo1 <- c(resultados_grupo1, paste('Media:', round(mean_feature, 3), '\n'))
+        resultados_grupo1 <- c(resultados_grupo1, paste('varianza:', round(var_feature, 3), '\n\n'))
     }
 }
 
 for(feature in datos_grupo2) {
     if(feature != 'chas') {
-        mean_feature = mean(grupo_2_chas[[feature]])
-        var_feature = var(grupo_2_chas[[feature]])
+        mean_feature <- mean(grupo_2_chas[[feature]])
+        var_feature <- var(grupo_2_chas[[feature]])
 
-        resultados_grupo2 = c(resultados_grupo2, paste(feature, ':\n', sep = ''))
-        resultados_grupo2 = c(resultados_grupo2, paste('Media:', round(mean_feature, 3), '\n'))
-        resultados_grupo2 = c(resultados_grupo2, paste('varianza:', round(var_feature, 3), '\n\n'))
+        resultados_grupo2 <- c(resultados_grupo2, paste(feature, ':\n', sep = ''))
+        resultados_grupo2 <- c(resultados_grupo2, paste('Media:', round(mean_feature, 3), '\n'))
+        resultados_grupo2 <- c(resultados_grupo2, paste('varianza:', round(var_feature, 3), '\n\n'))
     }
 }
 
@@ -172,12 +172,12 @@ cat("Resultados Grupo 2:\n", resultados_grupo2, sep = "")
 
 # Ejercicio 15
 
-features_data = names(data_boston_housing)
+features_data <- names(data_boston_housing)
 
 for(feature in features_data) {
     if(feature != 'chas') {
-        mean_feature = mean(data_boston_housing[[feature]])
-        var_feature = var(data_boston_housing[[feature]])
+        mean_feature <- mean(data_boston_housing[[feature]])
+        var_feature <- var(data_boston_housing[[feature]])
 
         cat(paste(feature, ':\n', sep = ''))
         cat(paste('Promedio:', round(mean_feature, 3), '\n'))
@@ -186,11 +186,11 @@ for(feature in features_data) {
 }
 
 # Ejercicio 16
-features_data = names(data_boston_housing)
+features_data <- names(data_boston_housing)
 
 for(feature in features_data) {
     if (feature != 'chas') {
-        var_inter = as.numeric(nrow(grupo_1_chas) / nrow(data_boston_housing)) * ((mean(grupo_1_chas[[feature]]) -
+        var_inter <- as.numeric(nrow(grupo_1_chas) / nrow(data_boston_housing)) * ((mean(grupo_1_chas[[feature]]) -
                     mean(data_boston_housing[[feature]]))^2 ) + (nrow(grupo_2_chas) / nrow(data_boston_housing)) *
                     ((mean(grupo_2_chas[[feature]]) - mean(data_boston_housing[[feature]]))^2)
 
@@ -200,11 +200,11 @@ for(feature in features_data) {
 }
 
 # Ejercicio 17
-features_data = names(data_boston_housing)
+features_data <- names(data_boston_housing)
 
 for(feature in features_data) {
     if (feature != 'chas') {
-        var_intra = (nrow(grupo_1_chas) / nrow(data_boston_housing)) * var(grupo_1_chas[[feature]]) +
+        var_intra <- (nrow(grupo_1_chas) / nrow(data_boston_housing)) * var(grupo_1_chas[[feature]]) +
                     (nrow(grupo_2_chas) / nrow(data_boston_housing)) * var(grupo_2_chas[[feature]])
 
         cat(paste(feature, ':\n'))
